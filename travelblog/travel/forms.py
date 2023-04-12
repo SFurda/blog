8fbeccab677d46601071adb693d6fad1bd152814
Svyatoslav Furda <svyatoslav.furda@gmail.com>
2023-04-11 21:03:43 -0500

    created new pages and added functionality. modified styles

--- travelblog/travel/forms.py
+++ travelblog/travel/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Travel
+from .models import Travel, Tag, Category
 
 class TravelForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Travel
         fields = ['title', 'description', 'youtube_url', 'image_url', 'category', 'tags']
--- travelblog/travel/views.py
+++ travelblog/travel/views.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
-from .models import Travel
+from .models import Travel, Category
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .forms import TravelForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -11,7 +13,20 @@ def index(request):
     posts = Travel.objects.all().order_by('-pub_date')
     return render(request, 'travel/index.html', {'posts': posts})
 
+def travel(request, category_slug=None):
+    categories = Category.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        posts = Travel.objects.filter(category=category).order_by('-pub_date')
+    else:
+        posts = Travel.objects.all().order_by('-pub_date')
+    return render(request, 'travel/travel.html', {'posts': posts, 'categories': categories})
+
+@login_required
 def create_post(request):
+    if not request.user.is_superuser:
+        return redirect(index)
+    
     if request.method == 'POST':
         form = TravelForm(request.POST)
         if form.is_valid():
@@ -20,7 +35,7 @@ def create_post(request):
             post.published_date = timezone.now()
             post.save()
             form.save_m2m()
-            return redirect(index)  # Replace with your post list URL pattern name
+            return redirect(index)  
     else:
         form = TravelForm()
     return render(request, 'travel/createpost.html', {'form': form})
